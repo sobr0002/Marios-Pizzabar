0e1ecb29df2a5d76240817166db2d7f7f5106a50
ayan0138 <ayah0002@stud.kea.dk>
2024-10-25 15:49:38 +0200

    OrderType 2.1 update

--- src/OrderType.java
+++ src/OrderType.java
@@ -1,19 +1,21 @@
 import java.time.LocalTime;
-
 public class OrderType {
    private LocalTime orderTime;
    private int ID;
 
+   //CONSTRUCTOR
 public OrderType(int ID) {
    this.ID = ID;
 
-}
+} //GETTER-METODE FOR AT INDHENTE BESTILLINGSTIDSPUNKT
    public LocalTime getorderTime() {
       return orderTime;
-}
+
+} //GETTER-METODE FOR AT INDHENTE ID
    public int getID() {
       return ID;
-}
+
+} //UDSKRIVE ORDER TIME OG ID VIA TOSTRING
    @Override
    public String toString() {
       return "Order Time: " + orderTime.toString() + ", ID: " + ID;
