560e4e8f2acfdc2c98933dbf14adbf74b90dde5a
sobr0002 <sobr0002@stud.kea.dk>
2024-10-25 13:26:15 +0200

    Marios Pizzabar

--- src/MenuItem.java
+++ src/MenuItem.java
@@ -4,6 +4,36 @@ public class MenuItem {
     private float price;
     private String name;
 
-    public
+    /*Setter attributterne direkte i konstruktøren for at sikre, at de får tildelt en nødvendig værdi fra starten
+    = sikrer initialisering af attributter */
+
+    public MenuItem(int itemNumber, float price, String name) {
+        this.setItemNumber(itemNumber);
+        this.setPrice(price);
+        this.setName(name);
+    }
+
+    //Getter- og setter-metoderne sikrer indkapsling
+    public int getItemNumber() {
+        return itemNumber;
+    }
+    public void setItemNumber(int itemNumber) {
+        this.itemNumber = itemNumber;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+    //setter sikrer justering af attribut
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
