01dc5aa03497b02289d8c3f242ba7785f0d32cbd
Gabel1998 <andreassgabel@hotmail.com>
2024-10-28 16:35:20 +0100

    Påbegybdt task 5.1, laver efterfølgemde task 5.2

---
+++ src/OrderPackage/Sort.java
@@ -0,0 +1,25 @@
+package OrderPackage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sort {
+
+
+    //GeeksForGeeks.com kaldte den kommende metode for bubble-sort metoden, men vi har lavet den i undervisningen også
+    public static void SorteringAfTid(ActiveOrders activeOrders){
+
+        //henter listen af bestillinger af ordrer fra ActiveOrder klassen
+        ArrayList<OrderType> activeOrderList = activeOrders.getActiveOrderlist();
+
+        int n = activeOrderList.size(); //størrelsen af listen defineres
+
+        //Kører igennem listen n-1 gange
+        for (int i = 0; i < n-1; i++){
+
+        }
+
+
+
+    }
+}
