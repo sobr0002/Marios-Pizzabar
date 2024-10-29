2e5545ef0d6e0920dc328e575f3c9f9910298276
lvnguyen14 <longvannguyen9@gmail.com>
2024-10-29 09:29:40 +0100

    test sprint 2

---
+++ src/OrderPackage/ActiveOrdersTest.java
@@ -0,0 +1,22 @@
+package OrderPackage;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ActiveOrdersTest {
+
+    static ArrayList<OrderType> activeOrderList;
+
+    @BeforeEach
+    void setup(){
+        activeOrderList = new ArrayList<>();
+    }
+
+    @Test
+
+
+}
